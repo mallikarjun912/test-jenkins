62043fd1b7c43fe4d8f3747880e1c8518eb29d77
Mallikarjun <rjun912@gmail.com>
2024-09-01 14:57:08 +0530

    added controller and changed port no in properties file

---
+++ src/main/java/sample/JenkinsController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Jenkins Tool";
+	}
+	
+}
