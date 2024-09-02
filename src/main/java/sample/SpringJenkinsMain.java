84a5d8c1c077f89d689a357b3bb5c3fa0f4df383
Mallikarjun <rjun912@gmail.com>
2024-09-02 14:48:53 +0530

    added logger feature

--- src/main/java/sample/SpringJenkinsMain.java
+++ src/main/java/sample/SpringJenkinsMain.java
@@ -1,13 +1,23 @@
 package sample;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsMain {
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsMain.class);
+	@PostConstruct
+	public void init() {
+		logger.info("Application started"); 
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		logger.info("application executed");  
 		SpringApplication.run(SpringJenkinsMain.class, args);
 	}
 
