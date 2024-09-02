4d2bca34b15ca17c36ea6011630b124a6bcef41b
Mallikarjun <rjun912@gmail.com>
2024-09-02 15:31:42 +0530

    removed junit4 jar from project

--- src/test/java/sample/SpringJenkinsMainTest.java
+++ src/test/java/sample/SpringJenkinsMainTest.java
@@ -1,22 +1,20 @@
 package sample;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJenkinsMainTest {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsMainTest.class);
 	@Test
 	public void testContextLoads() {
 		logger.info("test case executing...");  
-		//assertEquals(true, true); 
+		assertEquals(true, true); 
 	}
 
 }
